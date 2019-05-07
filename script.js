e399893191948d7587483264480b636e9a3ffd44
mcadcliff <44850474+mcadcliff@users.noreply.github.com>
2019-05-07 18:44:48 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+//var image = new Vue ({
+  //  el: "#image",
+//    data: {imageSrc="pfw/FINAL/8ball.jpg"}
+// });
+
+
+
+// Vue.component ("image-component",{
+   //     props: ['question'],
+   //     template:`
+   //     <div class="your-question">
+   //     <div>Your Question:<p>{{question.yourQuestion}}</p>
+    //    </div>`,
+    
+    //    data: function()
+   //     {return: {question: []} }
+// })
+
+// var vm = new Vue({
+ //  el: "app";
+ //  data: {
+ //   question: [{" "},]
+    //    }
+//  });
+
+
+
+// V-IF & V-ELSE FOR QUESTION ? // 
+//function questionMark (question) 
+//{
+   // if (question = "?") 
+   // {console.log(answer);}
+   // else {alert ("please use punctuation");}
+//}
+
+
+
+var question = "?";
+var n = question.endsWith("?");
+    
+// TOGGLE ANSWER // document.getElementById("question").classList.toggle("")
+
+
+    
+// THINKING LOOP ... ANIMATION //
+// var count = 0;
+// setInterval(function()
+        //  {count++;
+   //         var dots = new Array (count %10).join('.');
+       // document.getElementById('animation').innerHTML = "Hmm let me think" + dots;},
+    //     1000);
+    
+
+
+
+
+
+//const Button = { tagName: 'btn',
+              // init(btn){
+             //      btn.addEventListener('click',()=>{
+           //            btn.textContent++;
+            //       })
+          //     },
+          //     getAll(container = document.body){
+                   //return.container.getElementsByTagName(this.tagName);
+         //      }
+            //   };
