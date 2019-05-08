1f6b83643466a01795882977f9cfea1ce0d4ce31
mcadcliff <44850474+mcadcliff@users.noreply.github.com>
2019-05-07 19:53:41 -0500

    add background image animation

--- script.js
+++ script.js
@@ -3,7 +3,10 @@
 //    data: {imageSrc="pfw/FINAL/8ball.jpg"}
 // });
 
-
+// BACKGROUND IMAGE ANIMATION //
+//var ColourArray = ['#80bad9', '#ede9af', '#f2afdf', '#cdeb83', '#8bc3b1', '#f9d999', '#eeb550', '#CB99C9', '#F49AC2', '#779ECB', '#AEC6CF', '#DEA5A4', '#ab907a', '#FF6961', '#c8a99f', '#bec89f', '#99C7FF', '#ffbdbd'];
+          var getBGcolour = ColourArray[Math.floor(Math.random() * ColourArray.length)];
+          document.body.style.backgroundColor = getBGcolour;
 
 // Vue.component ("image-component",{
    //     props: ['question'],
@@ -26,6 +29,12 @@
 
 
 // V-IF & V-ELSE FOR QUESTION ? // 
+// var letter = // document.getElementById("questionMark").value;
+// var text;
+// if(letter === "?"){answer};
+// else {alert("please end your question with a question mark");}
+
+
 //function questionMark (question) 
 //{
    // if (question = "?") 
@@ -34,11 +43,20 @@
 //}
 
 
+// var question = "";
+// var btnText = question.endsWith("?");
 
-var question = "?";
-var n = question.endsWith("?");
+
+//function ifQuestion (?){
+//    if(question).endsWith ("?");
+//    return answer;}
+//else {return: "please end your question with a question mark..."}
     
-// TOGGLE ANSWER // document.getElementById("question").classList.toggle("")
+
+    
+// TOGGLE ANSWER // 
+//document.getElementById("question").classList.toggle("")
+
 
 
     
@@ -50,9 +68,6 @@ var n = question.endsWith("?");
        // document.getElementById('animation').innerHTML = "Hmm let me think" + dots;},
     //     1000);
     
-
-
-
 
 
 //const Button = { tagName: 'btn',
